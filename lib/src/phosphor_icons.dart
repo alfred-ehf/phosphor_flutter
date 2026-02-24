d88ea98a81050a0961eb2ee6f7cc82d46f2ad322
trejdych <rejdychtomasz@me.com>
2026-02-24 05:09:29 +0100

    remove light and thin weight

--- lib/src/phosphor_icons.dart
+++ lib/src/phosphor_icons.dart
@@ -4,6 +4,4 @@
 export 'package:phosphor_flutter/src/phosphor_icons_bold.dart';
 export 'package:phosphor_flutter/src/phosphor_icons_duotone.dart';
 export 'package:phosphor_flutter/src/phosphor_icons_fill.dart';
-export 'package:phosphor_flutter/src/phosphor_icons_light.dart';
 export 'package:phosphor_flutter/src/phosphor_icons_regular.dart';
-export 'package:phosphor_flutter/src/phosphor_icons_thin.dart';
